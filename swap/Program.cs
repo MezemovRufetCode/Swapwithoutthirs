26f91ce10a946e27010ca9e6cb8ed5296e28f382
rufatam <rufatam@CACER9>
2021-10-04 14:59:23 +0400

    Add project files.

---
+++ swap/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"a={a} b={b}");
+            a = a + b;  
+            b = a - b;
+            a = a - b;
+            Console.WriteLine($"a={a} b={b}");
+        }
+    }
+}
